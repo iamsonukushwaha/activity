c1f34319275f86b41cd707d8d4321050db53d25e
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-17 02:40:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,15 +20,14 @@ feednami.load(url, function (result) {
         for (var i = 0; i < entries.length; i++) {
             var entry = entries[i]
 
-            // console.log(entry.title)
-            // console.log(entry.summary)
+           
 
             let li = document.createElement('li');
             let para = document.createElement('p');
 
             //add HTML content to list items
             li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4>`;
-            para.innerHTML = entry.summary;
+            para.innerHTML = entry.content;
 
 
             //append HTML content to list 
